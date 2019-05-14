5bfdd94089c6af36c5ad17cada5006c1ff58defd
lvzm.lv <lvzm.lv@lvzmlvdeMacBook-Pro.local>
2019-05-14 15:51:57 +0800

    refactor

--- src/main/java/the/flash/protocol/PacketCodeC.java
+++ src/main/java/the/flash/protocol/PacketCodeC.java
@@ -54,39 +54,29 @@ public class PacketCodeC {
     public Packet decode(ByteBuf byteBuf) {
         // 跳过 magic number
         byteBuf.skipBytes(4);
-
         // 跳过版本号
         byteBuf.skipBytes(1);
-
         // 序列化算法
         byte serializeAlgorithm = byteBuf.readByte();
-
         // 指令
         byte command = byteBuf.readByte();
-
         // 数据包长度
         int length = byteBuf.readInt();
-
         byte[] bytes = new byte[length];
         byteBuf.readBytes(bytes);
-
         Class<? extends Packet> requestType = getRequestType(command);
         Serializer serializer = getSerializer(serializeAlgorithm);
-
         if (requestType != null && serializer != null) {
             return serializer.deserialize(requestType, bytes);
         }
-
         return null;
     }
 
     private Serializer getSerializer(byte serializeAlgorithm) {
-
         return serializerMap.get(serializeAlgorithm);
     }
 
     private Class<? extends Packet> getRequestType(byte command) {
-
         return packetTypeMap.get(command);
     }
 }
--- src/main/java/the/flash/server/ServerHandler.java
+++ src/main/java/the/flash/server/ServerHandler.java
@@ -10,24 +10,16 @@ import the.flash.protocol.response.LoginResponsePacket;
 
 import java.util.Date;
 
-/**
- * @author chao.yu
- * chao.yu@dianping.com
- * @date 2018/08/04 06:21.
- */
 public class ServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         System.out.println(new Date() + ": 客户端开始登录……");
         ByteBuf requestByteBuf = (ByteBuf) msg;
-
         Packet packet = PacketCodeC.INSTANCE.decode(requestByteBuf);
-
         if (packet instanceof LoginRequestPacket) {
             // 登录流程
             LoginRequestPacket loginRequestPacket = (LoginRequestPacket) packet;
-
             LoginResponsePacket loginResponsePacket = new LoginResponsePacket();
             loginResponsePacket.setVersion(packet.getVersion());
             if (valid(loginRequestPacket)) {
